e42366a378a67e32ad8b5a6cb340e136143e68b2
MS3300Studios <mikistrus@gmail.com>
2024-03-05 00:30:16 +0100

    fix server.js to use static assets and return index.html on all routes

--- server.js
+++ server.js
@@ -3,21 +3,28 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"))
-})
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+app.use('/assets', express.static(path.join(__dirname, 'frontend', 'assets')));
 
-app.get('/styles.css', (req, res) => {
-  res.sendFile(path.join(__dirname, "/styles.css"))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 app.get('/favicon.ico', (req, res) => {
   res.sendFile(path.join(__dirname, "/favicon.ico"))
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "/404.html"))
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"))
 })
+
+// app.get('/styles.css', (req, res) => {
+//   res.sendFile(path.join(__dirname, "/styles.css"))
+// })
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, "/404.html"))
+// })
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`)
