8869c20d7ed6eb60985a8d523d57d22966589eb0
Mikołaj Strusiński <mikolaj.strusinski@medius.com>
2024-03-05 14:52:51 +0100

    move icon to react bundle, update font

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,10 +1,23 @@
+import { useEffect } from "react" ;
 import classes from "./Layout.module.css";
+import faviconSvg from '../../assets/favicon.svg';
 
 interface IProps{
     children: React.ReactNode[] | React.ReactNode;
 }
 
 export const Layout = ({ children }: IProps) => {
+    useEffect(() => {
+        const favicon = document.createElement('link');
+        favicon.rel = 'icon';
+        favicon.type = 'image/svg+xml';
+        favicon.href = faviconSvg;
+        document.head.appendChild(favicon);
+
+        return () => {
+            document.head.removeChild(favicon);
+        };
+    }, [])
     return (
         <>
             <header>
