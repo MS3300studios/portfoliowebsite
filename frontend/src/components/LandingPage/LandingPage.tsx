4b85d44a593d90b9db018c564f1abdaa3cb5596a
MS3300Studios <mikistrus@gmail.com>
2024-04-07 00:16:30 +0200

    further landing page improvements (img, txt)

--- frontend/src/components/LandingPage/LandingPage.tsx
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -1,12 +1,16 @@
 import classes from "./LandingPage.module.css";
 import { useState, useEffect } from "react";
+import profilePicture from "../../assets/profile picture.jpg";
 
 export const LandingPage = () => {
-    const [isScrolled, setIsScrolled] = useState(false);
+    const [hasScrolledDown, setHasScrolledDown] = useState(false);
+    const [scrollPos, setScrollPos] = useState(window.scrollY);
+
     const handleScroll = () => {
         console.log("window.scrollY", window.scrollY)
-        if (window.scrollY > 50) {
-            setIsScrolled(true);
+        setScrollPos(window.scrollY)
+        if(window.scrollY > 50) {
+            setHasScrolledDown(true);
         }
     };
 
@@ -20,15 +24,25 @@ export const LandingPage = () => {
 
     return(
         <div className="center">
-            <div className={classes.headingWrapper} style={{opacity: isScrolled ? "0" : "100"}}>
-                <h1 className={classes.keyWords}>Inspiration</h1>
-                <h1 className={classes.keyWords}>Commitment</h1>
-                <h1 className={classes.keyWords}>Delivery</h1>
+            <div className={classes.headingWrapper} style={{opacity: scrollPos > 50 ? "0" : "100"}}>
+                <h1 className={classes.keyWords}>💡Inspiration</h1>
+                <h1 className={classes.keyWords}>✨Commitment</h1>
+                <h1 className={classes.keyWords}>🚚Delivery</h1>
             </div>
 
-            <article className={classes.article} style={{opacity: isScrolled ? "100" : "0"}}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus repudiandae, accusantium vitae, quibusdam et unde eaque, inventore accusamus nisi quis vel ab optio quisquam. Nobis quibusdam dolorum earum architecto aliquid voluptatem reiciendis magni repellat, numquam illum natus, quidem modi delectus cupiditate veniam ex repellendus ut voluptates similique nihil. Incidunt, voluptas.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores reiciendis unde accusantium ipsum consectetur possimus qui. Aliquam corporis accusamus asperiores quam veritatis earum fugiat. Accusantium quam vitae sequi vero?
+            <article className={classes.article} style={{opacity: hasScrolledDown ? "100" : "0"}}>
+                <div className={classes.profileWrapper}>
+                    <img src={profilePicture} alt="Mikolaj Strusiński" />
+                </div>
+                <div className={hasScrolledDown ? classes.innerArticle : ""}>
+                    <h1>
+                        Mikołaj Strusiński
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus repudiandae, accusantium vitae, quibusdam et unde eaque, inventore accusamus nisi quis vel ab optio quisquam. Nobis quibusdam dolorum earum architecto aliquid voluptatem reiciendis magni repellat, numquam illum natus, quidem modi delectus cupiditate veniam ex repellendus ut voluptates similique nihil. Incidunt, voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores reiciendis unde accusantium ipsum consectetur possimus qui. Aliquam corporis accusamus asperiores quam veritatis earum fugiat. Accusantium quam vitae sequi vero?
+                    </p>
+                </div>
             </article>
         </div>
     )
