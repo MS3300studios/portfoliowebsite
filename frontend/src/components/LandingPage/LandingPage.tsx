d342ab9459dc7bb8120d35450c3efa83f39bbaeb
MS3300Studios <mikistrus@gmail.com>
2025-01-10 01:44:11 +0100

    moved animation.js code into index.html and improved arrowdown

--- frontend/src/components/LandingPage/LandingPage.tsx
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -4,8 +4,17 @@ import DotNetIcon from "../../assets/dotNet";
 import ReactIcon from "../../assets/react";
 import TypescriptIcon from "../../assets/typescript";
 import ArrowDown from "../../assets/arrowDown";
+import { useEffect, useState } from "react";
 
 export const LandingPage = () => {
+    const [showArrowDown, setShowArrowDown] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setShowArrowDown(true)
+        }, 2600);
+    }, []);
+
     return (
         <div>
             <div className={classes.welcomeBannerContainer}>
@@ -20,7 +29,7 @@ export const LandingPage = () => {
                     </div>
                 </div>
             </div>
-            <div className={classes.scrollDownContainer}>
+            <div className={classes.scrollDownContainer} style={{opacity: showArrowDown ? "1" : "0"}}>
                 <ArrowDown />
             </div>
         </div>
